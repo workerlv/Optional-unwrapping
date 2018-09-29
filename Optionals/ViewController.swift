dd3bd67faf8c5e7e7c6f21487a4112bbecd13a57
Workerlv <arturs280@inbox.lv>
2018-09-29 09:19:37 +0300

    Simple example created

--- Optionals/ViewController.swift
+++ Optionals/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+struct Device {
+    var type: String
+    var price: Float
+    var color: String
+    
 }
 
+class ViewController: UIViewController {
+
+    var optionalNumber: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        optionalNumber = 2
+        
+        ifLetEx()
+        
+        guardEx(number: optionalNumber)
+        
+        
+        // optional chaining example
+        var phone: Device?
+        phone = Device(type: "Phone", price: 100, color: "Black")
+        
+        let devicePrice = phone?.price
+        
+        if let devicePrice = devicePrice {
+            print("Total price = \(devicePrice + 10)")
+        }
+        
+        
+    }
+    
+    func ifLetEx () {
+        
+        
+        if let number = optionalNumber {
+            print("optionalNumber = \(number)")
+        } else {
+            print("optionalNumber = nil")
+        }
+        
+    }
+
+    
+    func guardEx (number: Int?) {
+        
+        guard let tempNumber = number else {
+            print("No value - exiting func")
+            return
+        }
+        
+        print("Got number = \(tempNumber)")
+        
+    }
+
+    
+}
